fe02bb350f7cf8ad3826a8a37d3d774882af8c3e
Jubayth Hossen Roni <84420078+rony9843@users.noreply.github.com>
2023-04-11 10:26:53 +0300

    carousel and progessbar

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import NetInfo from "@react-native-community/netinfo";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { createContext, useEffect, useState } from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import BottomTabs from "./bottomTabsNavi";
 import RegisterScreen from "./screens/RegisterScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
@@ -15,8 +16,23 @@ export default function App() {
   const [loggingUserInfo, setLoginUserInfo] = useState({});
   const [pageState, setPageState] = useState("Welcome");
 
+  const [randomNumber, setRandomNumber] = useState(1);
+
+  let isConnected = false;
+
+  useEffect(() => {
+    unsubscribe();
+
+    if (isConnected === false) {
+      setTimeout(() => {
+        setRandomNumber(Math.floor(100000000 + Math.random() * 900000000));
+      }, 2000);
+    }
+  }, [randomNumber]);
+
   useEffect(() => {
     getUser();
+
     // removeValue();
   }, []);
 
@@ -38,21 +54,35 @@ export default function App() {
     setPageState(getUserFromStorage === false ? "Welcome" : "LandingPage");
   };
 
+  // Subscribe
+  const unsubscribe = NetInfo.addEventListener((state) => {
+    console.log("Connection type", state.type);
+    console.log("Is connected?", state.isConnected);
+
+    isConnected = state.isConnected;
+  });
+
   return (
     <UserInfoContext.Provider value={[loggingUserInfo, setLoginUserInfo]}>
-      <View style={{ flex: 1 }}>
-        <NavigationContainer>
-          <StatusBar style="auto" />
+      {isConnected ? (
+        <View style={{ flex: 1 }}>
+          <NavigationContainer>
+            <StatusBar style="auto" />
 
-          {pageState === "Welcome" ? (
-            <WelcomeScreen setPageState={setPageState} />
-          ) : pageState === "RegisterScreen" ? (
-            <RegisterScreen setPageState={setPageState} />
-          ) : (
-            <BottomTabs />
-          )}
-        </NavigationContainer>
-      </View>
+            {pageState === "Welcome" ? (
+              <WelcomeScreen setPageState={setPageState} />
+            ) : pageState === "RegisterScreen" ? (
+              <RegisterScreen setPageState={setPageState} />
+            ) : (
+              <BottomTabs />
+            )}
+          </NavigationContainer>
+        </View>
+      ) : (
+        <View>
+          <Text>Please connect internet</Text>
+        </View>
+      )}
     </UserInfoContext.Provider>
   );
 }
--- stackScreen/HomeStackScreen.js
+++ stackScreen/HomeStackScreen.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatListSlider } from "react-native-flatlist-slider";
+import { ProgressStep, ProgressSteps } from "react-native-progress-steps";
 import CatagoriesData from "../Data/CatagoriesData";
 import HomeCatagoriesBox from "../ui/HomeCatagoriesBox";
 
@@ -7,6 +9,24 @@ const HomeStackScreen = ({ navigation }) => {
   // onPress={() => navigation.navigate("Product")}
 
   const [selectCatagories, setSelectCatagories] = useState(11);
+  const width = Dimensions.get("window").width;
+
+  const images = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1567226475328-9d6baaf565cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+      desc: "Silent Waters in the mountains in midst of Himilayas",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1455620611406-966ca6889d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1130&q=80",
+      desc: "Red fort in India New Delhi is a magnificient masterpeiece of humans",
+    },
+  ];
+
+  const buttonTextStyle = {
+    color: "#393939",
+  };
 
   return (
     <View style={styles.container}>
@@ -33,6 +53,38 @@ const HomeStackScreen = ({ navigation }) => {
       </View>
       <View style={styles.carouselContainer}>
         <Text>This is carousel</Text>
+      </View>
+
+      <View>
+        <FlatListSlider data={images} />
+      </View>
+
+      <View>
+        <View>
+          <View style={{ flex: 1 }}>
+            <ProgressSteps activeStep={0} isComplete={true}>
+              <ProgressStep
+                removeBtnRow={true}
+                nextBtnDisabled={true}
+                label="First Step"
+              >
+                <View style={{ alignItems: "center" }}>
+                  <Text>Pending</Text>
+                </View>
+              </ProgressStep>
+              <ProgressStep label="Second Step">
+                <View style={{ alignItems: "center" }}>
+                  <Text>On The Way</Text>
+                </View>
+              </ProgressStep>
+              <ProgressStep label="Third Step">
+                <View style={{ alignItems: "center" }}>
+                  <Text>Complete</Text>
+                </View>
+              </ProgressStep>
+            </ProgressSteps>
+          </View>
+        </View>
       </View>
     </View>
   );
